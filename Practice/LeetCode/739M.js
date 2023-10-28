2cdc6cfb17756281f78d0bd1f829aed3f35cf3e1
BiangbiangOnlyOne <3122583749@qq.com>
2023-10-28 23:46:33 +0800

    '注册网页'

--- Practice/LeetCode/739M.js
+++ Practice/LeetCode/739M.js
@@ -1,3 +1,6 @@
+// 739.给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，
+// 下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+
 let temperatures =[73,74,75,71,69,72,76,73]
 // 输出[1,1,4,2,1,1,0,0]
 // var dailyTemperatures = function(temperatures) {
