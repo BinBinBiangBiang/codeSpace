08ab2c8b4fb22f30cc61cba77e69e189913e4e1d
BiangbiangOnlyOne <3122583749@qq.com>
2023-11-05 21:12:51 +0800

    1

---
+++ Practice/test/1.js
@@ -0,0 +1,10 @@
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+let arr3 = [5, 6];
+let binArr1 = arr1.concat(arr2);
+// merged为[1, 2, 3, 4]
+console.log(binArr1)
+
+let binArr2 = arr1.concat(arr2, arr3);
+console.log(binArr2)
+
