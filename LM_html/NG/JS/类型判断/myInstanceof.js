6b4bc9fb2564424272666020313cb2cbff14c6d3
BiangbiangOnlyOne <3122583749@qq.com>
2023-11-10 20:14:59 +0800

    instanceof 源码分析

---
+++ LM_html/NG/JS/类型判断/myInstanceof.js
@@ -0,0 +1,15 @@
+function myInstanceof(L,R){
+  while(L !== null){
+    if(L.__proto__ === R.prototype){
+      return true;
+    }else{
+      L = L.__proto__;
+    }
+  }
+  return false;
+}
+
+const arr = [1]
+
+console.log(myInstanceof(arr,Array))
+console.log(myInstanceof(arr,Object))
