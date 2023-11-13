d75bb154766d612a8cbe02d339a7d0c311850542
BiangbiangOnlyOne <3122583749@qq.com>
2023-11-13 21:06:50 +0800

    同步 异步

--- LM_html/NG/ES6/Promise/2.js
+++ LM_html/NG/ES6/Promise/2.js
@@ -16,16 +16,41 @@ function marry(){
   })
 }
 
-xq().then((res) => {
-  marry().then((res2) => {
-    bady();
-  });
+// xq()
+// .then((res) => {
+//   marry()
+// })
+// .then((res2) => {
+//   bady();
+// });
+
+xq()
+.then((res) => {
+  console.log(res);
+  return marry()
+})
+.then((res2) => {
+  return bady();
+})
+.then((res3) => {
+  bady2();
 })
 
 
+
 function bady(){
+  return new Promise((resolve ,reject) => {
+    setTimeout(() =>{
+      console.log('小龙女出生了！');
+      resolve()
+    },1000)
+  })
+}
+
+function bady2(){
   setTimeout(() =>{
-    console.log('小龙女出生！');
+    console.log('小龙二出生了！');
   },500);
 }
+
 
