7ba82111172e00255b9fadd18db2bdb11b269e07
BiangbiangOnlyOne <3122583749@qq.com>
2023-11-14 11:10:40 +0800

    类型判断案例

---
+++ Practice/test/5.js
@@ -0,0 +1,14 @@
+
+let a = 10;
+let b = 'Hello';
+let c = true;
+let d = undefined;
+let e = Symbol('hello');
+let f = null;
+
+console.log(typeof a);  // 输出: "number"
+console.log(typeof b);  // 输出: "string"
+console.log(typeof c);  // 输出: "boolean"
+console.log(typeof d);  // 输出: "undefined"
+console.log(typeof e);  // 输出: "symbol"
+console.log(typeof f);  // 输出: "object"
---
+++ Practice/test/6.js
@@ -0,0 +1,9 @@
+let obj = {};
+let arr = [1, 2, 3];
+let date = new Date();
+let fuc = function(){};
+
+console.log(typeof obj);   // 输出: "object"
+console.log(typeof arr);   // 输出: "object"
+console.log(typeof date);  // 输出: "object"
+console.log(typeof fuc);  // 输出: "function"
