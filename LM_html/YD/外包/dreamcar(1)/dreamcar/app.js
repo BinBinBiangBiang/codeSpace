e575f641c1c4714a7417f99fb2eaeea09f3b29c3
BiangbiangOnlyOne <3122583749@qq.com>
2023-11-18 17:12:22 +0800

    外包

--- LM_html/YD/外包/dreamcar(1)/dreamcar/app.js
+++ LM_html/YD/外包/dreamcar(1)/dreamcar/app.js
@@ -12,6 +12,7 @@ App({
       url: 'https://resources.ninghao.net/wxapp-case/db.json',
       success: (response) => {
         // console.log(response);
+        // 将获取到的数据复制到globalData
         Object.assign(this.globalData, response.data)
         // console.log(this,'-----')
       }
