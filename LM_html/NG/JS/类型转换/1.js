8736b6c9be8de2c0c4153b12395b8134ce1a74be
BiangbiangOnlyOne <3122583749@qq.com>
2023-11-21 21:09:51 +0800

    类型转换1

---
+++ LM_html/NG/JS/类型转换/1.js
@@ -0,0 +1,9 @@
+let num = 123
+let str ='hello world'
+let un = undefined
+let n = null
+let flag = false
+
+let big = 123n
+let sy = Symbol(123)
+
---
+++ LM_html/NG/JS/类型转换/2.js
@@ -0,0 +1,3 @@
+// console.log(Number({}));  // NaN
+// console.log(Number([])); // 0
+
