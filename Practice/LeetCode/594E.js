3a9547b9f3ab4a5e8598fdcfc2cf43cb6934248e
BiangbiangOnlyOne <3122583749@qq.com>
2023-11-22 23:18:42 +0800

    力扣594题

---
+++ Practice/LeetCode/594E.js
@@ -0,0 +1,90 @@
+// 594.和谐数组是指一个数组里元素的最大值和最小值之间的差别 正好是 1 。
+
+// 现在，给你一个整数数组 nums ，请你在所有可能的子序列中找到最长的和谐子序列的长度。
+
+// 数组的子序列是一个由数组派生出来的序列，它可以通过删除一些元素或不删除元素、且不改变其余元素的顺序而得到。
+
+// 示例 1：
+
+// 输入：nums = [1,3,2,2,5,2,3,7]
+// 输出：5
+// 解释：最长的和谐子序列是 [3,2,2,2,3]
+// 示例 2：
+
+// 输入：nums = [1,2,3,4]
+// 输出：2
+// 示例 3：
+
+// 输入：nums = [1,1,1,1]
+// 输出：0
+
+// 排序 查找
+var findLHS1 = function(nums) {
+  nums.sort((a, b) => a - b);
+  let begin = 0;
+  let res = 0;
+  for (let end = 0; end < nums.length; end++) {
+      while (nums[end] - nums[begin] > 1) {
+          begin++;
+      }
+      if (nums[end] - nums[begin] === 1) {
+          res = Math.max(res, end - begin + 1);
+      }
+  }
+  return res;
+};
+
+// 直接循环查找
+var findLHS = function (nums) {
+  const len = nums.length;
+  let max = 0;
+  let n = 1;
+  let m = 0;
+  let sum = 0;
+  for (let i = 0; i < len; i++) {
+      n = 1;
+      m = 0;
+      let right = i + 1;
+      const curNum = nums[i];
+      while (right <= len - 1) {
+          if (nums[right] - curNum == 1)  {
+            console.log(1);
+              n++;
+          }
+          if(nums[right] == curNum){
+              console.log(2);
+              m++;
+          }
+          right++
+      }
+      right = i + 1;
+      if(n>=2){
+          sum = n+m;
+      }
+      max = Math.max(max, sum);
+      sum = 0;
+      n = 1;
+      m = 0;
+      while (right <= len - 1) {
+          if (nums[right] - curNum == -1 ) {
+              n++;
+              console.log(3);
+          }
+          if(nums[right] == curNum){
+              m++;
+              console.log(4);
+          }
+          right++
+      }
+      if(n>=2){
+          sum = n+m;
+      }
+      max = Math.max(max, sum);
+      console.log("i=", i+" max=", max);
+  }
+  return max;
+};
+
+nums = [1,2,1,2,1,2,1,2];
+
+console.log(findLHS(nums));
