1044c4e95841dfaebe0e9c1307bcbff21fed93aa
BinBinBiangBiang <3122583749@qq.com>
2023-12-01 22:40:04 +0800

    vue修改

--- LM_html/NG/LEARN_VUE/08_综合案例/index.js
+++ LM_html/NG/LEARN_VUE/08_综合案例/index.js
@@ -8,34 +8,35 @@ Vue.createApp({
           name: '《算法导论》',
           date: '2006-9',
           price: 85.00,
-          count: 1
+          count: 0
         },
         {
           id: 2,
           name: '《UNIX编程艺术》',
           date: '2006-2',
           price: 59.00,
-          count: 1
+          count: 0
         },
         {
           id: 3,
           name: '《编程珠玑》',
           date: '2008-10',
           price: 39.00,
-          count: 1
+          count: 0
         },
         {
           id: 4,
           name: '《代码大全》',
           date: '2006-3',
           price: 128.00,
-          count: 1
+          count: 0
         },
       ],
     }
   },
   computed: {
     totalPrice() {
+      // 两种计算价格的方式都可以
       // return this.books.reduce((sum, book) => {
       //   return sum + book.price * book.count;
       // }, 0);
@@ -51,7 +52,7 @@ Vue.createApp({
       this.books[index].count++;
     },
     down(index) {
-      if (this.books[index].count != 1) {
+      if (this.books[index].count != 0) {
         this.books[index].count--;
       }
     },
