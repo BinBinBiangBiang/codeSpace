dd4443ac266db56f7dbf23129934d1b73bc977da
BinBinBiangBiang <3122583749@qq.com>
2023-12-06 21:56:46 +0800

    element-ui

---
+++ LM_html/Vue/element-ui/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('@/views/Home.vue')
+  }
+  // {
+  //   path: '/home',
+  //   name: 'home',
+  //   component: () => import('@/views/Home.vue')
+  // }
+  //  {
+  //   path: '/home/:user',
+  //   component: () => import('@/views/Home.vue')
+  //  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
