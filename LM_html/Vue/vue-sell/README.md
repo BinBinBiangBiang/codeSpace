cca5f30aa48e816db21e7aaa979e84bb1eae4fdb
BinBinBiangBiang <3122583749@qq.com>
2023-12-11 21:35:18 +0800

    vue-sell

---
+++ LM_html/Vue/vue-sell/README.md
@@ -0,0 +1,7 @@
+# 项目开发流程
+1. 创建全局的css文件 添加reset.css(在main.js中引入)
+
+# 组件化
+1. import 引入
+2. components 注册
+3. 使用
---
+++ LM_html/Vue/vue-sell/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-header/>
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue';
+export default{
+  components:{
+    'v-header':Header,
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
