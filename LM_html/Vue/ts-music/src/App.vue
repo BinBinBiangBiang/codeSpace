28517f8dd557f84c50ba8cb05af01d174efd3b38
BinBinBiangBiang <3122583749@qq.com>
2023-12-23 12:13:17 +0800

    ts初学

---
+++ LM_html/Vue/ts-music/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted,ref } from 'vue';
+import { useBanner }  from './api/index';
+import type { Banner } from './models/banner';
+
+
+let banners = ref<Banner[]>([]) // 最简单的vue响应式
+
+onMounted(async() =>{
+  banners.value = await useBanner()
+  console.log(banners.value);
+  
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="banner in banners" :key="banner.targetId">
+      <img :src="banner.pic">
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
