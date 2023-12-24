6a8821b2148fc78f01ef8867eae6cf9b10392f1c
BinBinBiangBiang <3122583749@qq.com>
2023-12-24 23:26:02 +0800

    项目预备

---
+++ LM_html/MyObject/ManageSystem/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-view/>
+</template>
+
+<style >
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ LM_html/MyObject/ManageSystem/src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="common-layout">
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-header>Header</el-header>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
