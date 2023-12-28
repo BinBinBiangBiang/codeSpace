dd1299ac2bef321bfc1f17cfd5021a6dc5f832af
BinBinBiangBiang <3122583749@qq.com>
2023-12-28 21:33:51 +0800

    todos-max

---
+++ LM_html/Vue/todos-max/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="todo" :class="{ completed: todo.done}">
+    <div class="view">
+      <label  v-text="todo.text"></label>
+    </div>
+  </li>
+</template>
+
+<script setup>
+defineProps({
+  todo: {
+    type:Object,
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
