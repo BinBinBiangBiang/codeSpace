ec4f3fdadbc9362c3abf1cc79c6725cd54cf81a7
BinBinBiangBiang <3122583749@qq.com>
2024-01-02 21:22:51 +0800

    全栈项目一:前端

---
+++ LM_html/Vue/NOTE-BOOK/client/README.md
@@ -0,0 +1,2 @@
+1. 移动端适配  npm add amfe-flexible
+2. 常见标签样式初始化  
---
+++ LM_html/Vue/NOTE-BOOK/client/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'amfe-flexible' // 自动设置根字体大小
+import router from './router/index.js'
+import './assets/style/reset.css'
+import { Form, Field, CellGroup } from 'vant';
+
+// 1. 引入你需要的组件
+import { Button } from 'vant';
+// 2. 引入组件样式
+import 'vant/lib/index.css';
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(Button);
+app.use(Form);
+app.use(Field);
+app.use(CellGroup);
+  
+app.mount('#app')
