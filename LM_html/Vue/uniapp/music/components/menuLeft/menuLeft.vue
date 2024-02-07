11a585527511360e4e4c38e68f7e22210f99bd7d
BinBinBiangBiang <3122583749@qq.com>
2024-02-07 16:16:34 +0800

    fix:主页左侧弹出菜单

--- LM_html/Vue/uniapp/music/components/menuLeft/menuLeft.vue
+++ LM_html/Vue/uniapp/music/components/menuLeft/menuLeft.vue
@@ -4,7 +4,7 @@
 		<view class="menu-hd">
 			<view class="user-info">
 				<view class="avatar">
-					<image src="../../static/logo.png" mode="aspectFill" class="pic"></image>
+					<image class="pic" src="../../static/logo.png" mode="aspectFill"></image>
 				</view>
 				<view class="username">
 					<text>蜗牛</text>
@@ -16,44 +16,46 @@
 				<uni-icons type="scan" size="24"></uni-icons>
 			</view>
 		</view>
-		<view class="menu-bd"></view>
+
+		<view class="menu-bd">hello</view>
 	</view>
+
 	<view class="menu-mask" @click="hideMenu" v-show="isShow"></view>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-import { useStore } from 'vuex';
+import { computed, ref } from 'vue';
+import { useStore } from 'vuex'
 
-const store = useStore();
-
+const store = useStore()
+// console.log(store);
 const isShow = computed(() => {
-	return store.state.isShowMenu;
-});
+	return store.state.isShowMenu
+})
 
 const hideMenu = () => {
-	// isShow = false;
-	store.commit('changeIsShowMenu', false);
-};
+	// isShow.value = false
+	store.commit('changeIsShowMenu', false)
+}
 </script>
 
 <style lang="scss" scoped>
 .menu-left {
 	position: fixed;
-	top: 0;
+	top: 80rpx;
 	z-index: 9999;
 	width: 620rpx;
 	height: 100%;
 	padding: 30rpx;
 	box-sizing: border-box;
 	background-color: #fff;
-	box-shadow: 5px 0 20rpx #eee;
-	transition: all 0.4s ease-in-out;
-	&.show {
+	// box-shadow: 5px 0 20rpx #eee;
+	transition: all 0.5s ease-in-out;
+	&.show{
 		left: 0;
 	}
-	&.hide {
-		left: -640rpx;
+	&.hide{
+		left: -620rpx;
 	}
 	.menu-hd {
 		display: flex;
@@ -78,13 +80,14 @@ const hideMenu = () => {
 		}
 	}
 }
+
 .menu-mask {
 	position: fixed;
 	top: 0;
 	right: 0;
-	left: 0;
 	bottom: 0;
+	left: 0;
 	background-color: rgba(0, 0, 0, 0.5);
-	z-index: 999;
+	z-index: 1000;
 }
 </style>
