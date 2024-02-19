ab224a3a00871c9b4b7c94bfeff58fb8c9891dd6
BinBinBiangBiang <3122583749@qq.com>
2024-02-19 17:54:20 +0800

    reactive源码学习

---
+++ LM_html/Vue/mini-vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>{{ state.count }}</p>
+    <p>{{ num }}</p>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from './reactivity/reactive.js'
+import { computed, watch, effect } from 'vue'
+
+const state = reactive({
+  count: 1,
+  msg: 2
+})
+
+
+const num = computed(() => {
+  return state.count * 10
+})
+const state1 = reactive({
+  num: 1,
+})
+
+const add = () => {
+  state.count++
+}
+
+effect(() => {   // 类似生命周期一开始就执行  第二个参数为lazy,当值为true时，不会立即执行
+  console.log('hello world');
+}, { lazy: false })
+
+watch(() => state.count, () => {
+  (newVal, oldVal) => {
+    console.log(newVal, oldVal);
+  }
+})
+
+</script>
+
+<style lang="less" scoped></style>
---
+++ LM_html/Vue/mini-vue/src/reactivity/effect.js
@@ -0,0 +1,63 @@
+// 副作用收集
+const targetMap =new WeakMap();
+let activeEffect = null;
+
+export function effect(fn,options={}){
+  const effeftFn = () =>{
+    try{
+      activeEffect = effeftFn;
+      return fn()
+    }finally{
+      activeEffect = null;
+    }
+  }
+  if(!options.lazy){
+    effeftFn();
+  }
+  return effeftFn
+}
+
+// 为某个属性添加 effect
+export function track(target,key){
+  // targetMap = {  // 存成这样
+  //   target: {
+  //     key:[effect1,effect2,effect3,...]
+  //   },
+  //   target2: {
+  //     key:[effect1,effect2,effect3,...]
+  //   },
+  // }
+
+  let depsMap = targetMap.get(target);
+
+  if(!depsMap){ // 初次读取到值 收集effect
+    depsMap = new Map();
+    targetMap.set(target,depsMap);
+  }
+
+  let deps = depsMap.get(key);
+  if(!deps){ // 该属性还未添加任何effect
+    deps = new Set();
+  }
+  if(!deps.has(activeEffect) && activeEffect){
+    // 存入一个effect函数
+    deps.add(activeEffect)
+  }
+
+  depsMap.set(key,deps)
+}
+
+// 触发某个属性 effect
+export function trigger(target, key){
+  const depsMap = targetMap.get(target);
+  if(!depsMap){ // 该属性没有添加任何effect
+    return
+  }
+  const deps = depsMap.get(key);
+  if(!deps){ // 该属性没有添加任何依赖
+    return
+  }
+  deps.forEach(effect => {  // 将该属性上的所有的副作用函数全部触发
+    effect();
+  })
+}
