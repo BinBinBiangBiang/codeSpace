087562b1c89032043c361db9bd6f9134c2077a60
BinBinBiangBiang <3122583749@qq.com>
2024-02-19 23:36:41 +0800

    力扣第104题

---
+++ Practice/LeetCode/104E.js
@@ -0,0 +1,54 @@
+// 104. 二叉树的最大深度
+// 已解答
+// 简单
+// 相关标签
+// 相关企业
+// 给定一个二叉树 root ，返回其最大深度。
+
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+ 
+
+// 示例 1：
+
+
+
+ 
+
+// 输入：root = [3,9,20,null,null,15,7]
+// 输出：3
+// 示例 2：
+
+// 输入：root = [1,null,2]
+// 输出：2
+ 
+
+// 提示：
+
+// 树中节点的数量在 [0, 104] 区间内。
+// -100 <= Node.val <= 100
+
+
+// C++:
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+//   public:
+//       int maxDepth(TreeNode* root) {
+//           if(root == nullptr){
+//               return 0;
+//           }
+//           int left = maxDepth(root->left);
+//           int right = maxDepth(root->right);
+//           return max(left,right)+1;
+//       }
+//   };
