1258982d4146529aec33f77a91d45d7864be4df5
BinBinBiangBiang <3122583749@qq.com>
2024-02-24 00:07:05 +0800

    力扣第102题

---
+++ Practice/LeetCode/102M.js
@@ -0,0 +1,63 @@
+// 102. 二叉树的层序遍历
+// 已解答
+// 中等
+// 相关标签
+// 相关企业
+// 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+ 
+
+// 示例 1：
+
+
+// 输入：root = [3, 9, 20, null, null, 15, 7]
+// 输出：[[3], [9, 20], [15, 7]]
+// 示例 2：
+
+// 输入：root = [1]
+// 输出：[[1]]
+// 示例 3：
+
+// 输入：root = []
+// 输出：[]
+
+
+// 提示：
+
+// 树中节点数目在范围[0, 2000] 内
+//   - 1000 <= Node.val <= 1000
+
+// C++:
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+//   public:
+//       vector<vector<int>> levelOrder(TreeNode* root) {
+//           vector<vector<int>> result;
+//           queue<TreeNode*> que;
+//           if(root == NULL) return result;
+//           que.push(root);
+//           while(!que.empty()){
+//               int size = que.size();
+//               vector<int> vec;
+//               while(size--){
+//                   TreeNode* cur = que.front();
+//                   que.pop();
+//                   vec.push_back(cur->val);
+//                   if(cur->left) que.push(cur->left);
+//                   if(cur->right) que.push(cur->right);
+//               }
+//               result.push_back(vec);
+//           }
+//           return result;
+//       }
+//   };
