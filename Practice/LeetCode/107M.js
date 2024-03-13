2bf02132fe5b25fb747f78a8b900eac178e82f58
BinBinBiangBiang <3122583749@qq.com>
2024-03-13 23:45:51 +0800

    力扣107二刷

--- Practice/LeetCode/107M.js
+++ Practice/LeetCode/107M.js
@@ -63,3 +63,34 @@
 //           return result;
 //       }
 //   };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  const result = [];
+  if(!root) return result;
+  const queue = [];
+  queue.push(root);
+  while(queue.length){
+      let size = queue.length;
+      result.unshift([]);
+      for(let i = 0 ;i < size ;i++){
+          let node = queue.shift();
+          result[0].push(node.val);
+          if(node.left) queue.push(node.left);
+          if(node.right) queue.push(node.right);
+      }
+  }
+  return result;
+};
